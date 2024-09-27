5bab7e58b5e487eb4189e47ef312bba99e8a0a21
chetan anand <121672141+chetanuser@users.noreply.github.com>
2024-09-27 13:44:09 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 
-bot_token = ""
+bot_token = "6306140038:AAFje9Vxpp7ilL7x-b3wZFfSgwilNIeWG9c"
 admin_user_id = 5470956337
 required_channels = ['@BlackScripter', '@WingoGiftCodess', '@apex_91']
 logo_url = "https://ibb.co/R4sZgmJ"
