19175342d817d699f08c4f5a21e5f9aa17d5c7eb
chetan anand <121672141+chetanuser@users.noreply.github.com>
2024-09-27 13:43:48 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
 
-bot_token = "5229400507:AAHiTWbs45DgXa_8KsmS2eoPf633rU1_qOM"
+bot_token = ""
 admin_user_id = 5470956337
-required_channels = ['@tpviehuhs']
+required_channels = ['@BlackScripter', '@WingoGiftCodess', '@apex_91']
 logo_url = "https://ibb.co/R4sZgmJ"
 ref_bonus = 1
 reflink="https://82lottery7.com/#/register?invitationCode=887611939965"
